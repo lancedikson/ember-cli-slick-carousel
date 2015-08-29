9cae2755eb0a60ed1ba584c48c77da19201e4b74
Igor Preston <igorpreston@gmail.com>
2015-08-29 10:35:15 +0300

    added main index blueprint

--- index.js
+++ index.js
@@ -2,5 +2,35 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-cli-slick-carousel'
+  name: 'ember-cli-slick-carousel',
+
+  afterInstall: function() {
+    return this.addBowerPackagesToProject([
+      { name: 'slick-carousel', target: '1.5.8' }
+    ]);
+  },
+
+  included: function(app) {
+    this._super.included(app);
+
+    app.import(app.bowerDirectory + '/slick-carousel/slick/slick.js');
+    app.import(app.bowerDirectory + '/slick-carousel/slick/slick.css');
+    app.import(app.bowerDirectory + '/slick-carousel/slick/slick-theme.css');
+    app.import(app.bowerDirectory + '/slick-carousel/slick/ajax-loader.gif', {
+      destDir: 'assets'
+    });
+    app.import(app.bowerDirectory + '/slick-carousel/slick/fonts/slick.eot', {
+      destDir: 'assets/fonts'
+    });
+    app.import(app.bowerDirectory + '/slick-carousel/slick/fonts/slick.svg', {
+      destDir: 'assets/fonts'
+    });
+    app.import(app.bowerDirectory + '/slick-carousel/slick/fonts/slick.ttf', {
+      destDir: 'assets/fonts'
+    });
+    app.import(app.bowerDirectory + '/slick-carousel/slick/fonts/slick.woff', {
+      destDir: 'assets/fonts'
+    });
+
+  }
 };
